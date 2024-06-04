8e3459637b7c409ae6a93a842849d4fd0e9269a7
VISHNU <Vishnuvishva2434@gmail.com>
2024-06-04 21:09:21 +0530

    Update __main__.py

--- colab_leecher/__main__.py
+++ colab_leecher/__main__.py
@@ -437,7 +437,7 @@ async def help_command(client, message):
                 [
                     InlineKeyboardButton(  # Opens a web URL
                         "Channel 📣",
-                        url="https://t.me/Colab_Leecher",
+                        url="https://t.me/MR_V_BOTS",
                     ),
                     InlineKeyboardButton(  # Opens a web URL
                         "Group 💬",
